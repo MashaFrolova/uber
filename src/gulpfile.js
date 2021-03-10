cfbba4465a2bc5478a42ead7be0fde7b4a0274d3
Masha <manjunyafrolova@gmail.com>
2021-03-10 13:10:41 +0300

    gulp problem

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -14,25 +14,25 @@ gulp.task('server', function() {
     });
 });
 
-gulp.task('styles', function() {
-    return gulp.src("src/sass/*.+(scss|sass)")
-            .pipe (sass({outputStyle: 'compressed'}).on('error', sass.logError))
-            .pipe(rename({
-                prefix: "",
-                suffix: ".min",
-              }))
-            .pipe(autoprefixer({
-                browsers: ['last 2 versions'],
-                cascade: false
-            }))
-            .pipe(gulp.dest('dist'))
-            .pipe (gulp.dest(src/css))
-            .pipe (browserSync.stream());
-});
-            .pipe(cleanCSS({compatibility: 'ie8'}))
-gulp.task('watch', function() {
-    gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"))
-    gulp.watch("src/*.html").on ("change", browserSync.reload);
-});
+// gulp.task('styles', function() {
+//     return gulp.src("src/sass/*.+(scss|sass)")
+//             .pipe (sass({outputStyle: 'compressed'}).on('error', sass.logError))
+//             .pipe(rename({
+//                 prefix: "",
+//                 suffix: ".min",
+//               }))
+//             .pipe(autoprefixer({
+//                 browsers: ['last 2 versions'],
+//                 cascade: false
+//             }))
+//             .pipe(gulp.dest('dist'))
+//             .pipe (gulp.dest(src/css))
+//             .pipe (browserSync.stream())
+//             .pipe(cleanCSS({compatibility: 'ie8'}))
+// });
+// gulp.task('watch', function() {
+//     gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"))
+//     gulp.watch("src/*.html").on ("change", browserSync.reload);
+// });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+// gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
