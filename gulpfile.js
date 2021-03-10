2b09eb845a86fba33988304a3161272dc2921f84
Masha <manjunyafrolova@gmail.com>
2021-03-10 13:19:47 +0300

    gulp ok

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
 const sass = require('gulp-sass');
+const rename = require("gulp-rename");
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
 
 // Static server
 gulp.task('server', function() {
@@ -14,13 +17,22 @@ gulp.task('server', function() {
 gulp.task('styles', function() {
     return gulp.src("src/sass/*.+(scss|sass)")
             .pipe (sass({outputStyle: 'compressed'}).on('error', sass.logError))
+            .pipe(rename({
+                prefix: "",
+                suffix: ".min",
+              }))
+            .pipe(autoprefixer({
+                browsers: ['last 2 versions'],
+                cascade: false
+            }))
+            .pipe(gulp.dest('dist'))
             .pipe (gulp.dest(src/css))
-            .pipe (browserSync.stream());
-})
-
+            .pipe (browserSync.stream())
+            .pipe(cleanCSS({compatibility: 'ie8'}))
+});
 gulp.task('watch', function() {
     gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"))
     gulp.watch("src/*.html").on ("change", browserSync.reload);
-}
+});
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
